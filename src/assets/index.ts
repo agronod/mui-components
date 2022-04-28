61ca4ce8f5c356dffc4c4c3916681c5a2b854fdf
robinsmedberg <robin.smedberg@gmail.com>
2022-04-28 16:08:30 +0200

    add CheckCircleIcon from design

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,4 +1,5 @@
 import agrosfar from "./agrosfar.png";
 import header_background from "./header_background.png";
+import { ReactComponent as CheckCircleIcon } from "./CheckCircleIcon.svg";
 
-export { agrosfar, header_background };
+export { agrosfar, header_background, CheckCircleIcon };
