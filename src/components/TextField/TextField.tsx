2b1ab66c7f8a2a22fd6bb3711ede59afdfa1e722
robinsmedberg <robin.smedberg@gmail.com>
2022-05-02 21:05:52 +0200

    fix: make textfield with error same size as with no error

--- src/components/TextField/TextField.tsx
+++ src/components/TextField/TextField.tsx
@@ -1,13 +1,25 @@
 import {
   InputAdornment,
+  styled,
   TextField as MuiTextField,
   TextFieldProps,
 } from "@mui/material";
 import { ErrorIcon } from "../../assets";
 
+const StyledTextField = styled(MuiTextField)(({ theme, error, variant }) => ({
+  "& .MuiInputBase-input": {
+    width:
+      error && `calc(100% - ${theme.spacing(variant !== "standard" ? 4 : 3)});`,
+  },
+  "& .MuiInputAdornment-root": {
+    position: error && "absolute",
+    right: error && variant !== "standard" ? theme.spacing(1) : 0,
+  },
+}));
+
 export default function TextField(props: TextFieldProps) {
   return (
-    <MuiTextField
+    <StyledTextField
       {...props}
       InputProps={{
         endAdornment: props.error && (
@@ -16,6 +28,6 @@ export default function TextField(props: TextFieldProps) {
           </InputAdornment>
         ),
       }}
-    ></MuiTextField>
+    ></StyledTextField>
   );
 }
