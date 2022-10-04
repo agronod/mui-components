0a8c248bc6c74bbbe591f9df897fa79ef5bfd314
tolgakoyluoglu <42962297+tolgakoyluoglu@users.noreply.github.com>
2022-10-04 14:29:53 +0200

    feat: add header component (#3)

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -2,5 +2,12 @@ import agrosfar from "./agrosfar.png";
 import header_background from "./header_background.png";
 import { ReactComponent as CheckCircleIcon } from "./CheckCircleIcon.svg";
 import { ReactComponent as ErrorIcon } from "./ErrorIcon.svg";
+import AgrosfarPilotLogo from "./AgrosfarPilotLogo";
 
-export { agrosfar, header_background, CheckCircleIcon, ErrorIcon };
+export {
+  agrosfar,
+  header_background,
+  CheckCircleIcon,
+  ErrorIcon,
+  AgrosfarPilotLogo,
+};
