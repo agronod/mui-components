5b7cceb8696fb6d0fa5c9a441dc2f1f773b7d7b1
Tolga Köyluoglu <tolga.koyluoglu1@gmail.com>
2022-10-04 11:24:23 +0200

    feat: add header component

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,6 +2,14 @@ import { getColorScheme } from "./Theme/utils";
 import ThemeProvider, { useTheme } from "./Theme/ThemeProvider";
 import { SquareChip } from "./Chip";
 import { TextField } from "./TextField";
+import { Header } from "./Header";
 
-export { getColorScheme, ThemeProvider, SquareChip, TextField, useTheme };
+export {
+  getColorScheme,
+  ThemeProvider,
+  SquareChip,
+  TextField,
+  useTheme,
+  Header,
+};
 export default ThemeProvider;
