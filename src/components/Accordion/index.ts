280791c76dc148464b2b20d47291b9ab49d7d9a3
tolgakoyluoglu <42962297+tolgakoyluoglu@users.noreply.github.com>
2023-01-18 11:19:02 +0100

    feat: add accordion components (#16)

---
+++ src/components/Accordion/index.ts
@@ -0,0 +1,5 @@
+import Accordion from "./Accordion";
+import AccordionSummary from "./AccordionSummary";
+import AccordionDetails from "./AccordionDetails";
+
+export { Accordion, AccordionSummary, AccordionDetails };
