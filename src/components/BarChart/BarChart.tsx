614e3c0f71eab454e3793ed51fdc9ae1148ca793
robinsmedberg <robin.smedberg@gmail.com>
2023-02-22 15:00:56 +0100

    children on bar charts is optional

--- src/components/BarChart/BarChart.tsx
+++ src/components/BarChart/BarChart.tsx
@@ -22,7 +22,7 @@ export type BarChartProps = {
   yAxisOptions?: Partial<YAxisProps>;
   barOptions?: Partial<BarProps>;
   barCellsOptions?: Partial<CellProps[]>;
-  children: React.ReactNode;
+  children?: React.ReactNode;
 };
 
 export const BarChart: React.FC<BarChartProps> = ({
