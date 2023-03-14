9ba572bc3f622a8c7a5d2cb4d9188e56d7c4da46
robinsmedberg <robin.smedberg@gmail.com>
2023-03-14 22:05:02 +0100

    move to 1 index file

---
+++ src/index.ts
@@ -0,0 +1,73 @@
+import { SquareChip } from "./components/Chip";
+import { TextField } from "./components/TextField";
+import { Header } from "./components/Header";
+import { BarChart } from "./components/BarChart";
+import {
+  Nyckeltal,
+  NyckeltalGrid,
+  getNyckeltalVarde,
+  NyckeltalProps,
+} from "./components/Nyckeltal";
+import { Spinner } from "./components/Spinner";
+import {
+  Accordion,
+  AccordionDetails,
+  AccordionSummary,
+} from "./components/Accordion";
+import { Alert, AlertTitle } from "./components/Alert";
+import { VerticalStepper } from "./components/Stepper";
+import {
+  getColorScheme,
+  pxToRem,
+  ThemeProvider,
+  useTheme,
+} from "./components/Theme";
+import {
+  ErrorBoundary,
+  useErrorHandling,
+  ErrorMessage,
+} from "./components/ErrorBoundary";
+import {
+  agrosfar,
+  header_background,
+  CheckCircleIcon,
+  ErrorIcon,
+  AgrosfarPilotLogo,
+  AgronodLogo,
+  ErrorIconLarge,
+  WarningIcon,
+} from "./assets";
+
+export {
+  Alert,
+  AlertTitle,
+  VerticalStepper,
+  getColorScheme,
+  ThemeProvider,
+  SquareChip,
+  TextField,
+  useTheme,
+  Header,
+  BarChart,
+  Spinner,
+  Nyckeltal,
+  NyckeltalGrid,
+  getNyckeltalVarde,
+  Accordion,
+  AccordionSummary,
+  AccordionDetails,
+  pxToRem,
+  ErrorBoundary,
+  useErrorHandling,
+  ErrorMessage,
+  agrosfar,
+  header_background,
+  CheckCircleIcon,
+  ErrorIcon,
+  AgrosfarPilotLogo,
+  AgronodLogo,
+  ErrorIconLarge,
+  WarningIcon,
+};
+
+export type { NyckeltalProps };
