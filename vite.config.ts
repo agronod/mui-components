7728e9a5e338d31f84728d3822663d6dd4a901bb
robinsmedberg <robin.smedberg@gmail.com>
2023-03-15 21:11:15 +0100

    cleaning

--- vite.config.ts
+++ vite.config.ts
@@ -21,31 +21,6 @@ export default defineConfig({
           tsconfig: "./tsconfig-build.json",
         }),
       ],
-      // output: {
-      //   globals: {
-      //     react: "React",
-      //     "react-dom": "react-dom",
-      //     "react-router-dom": "react-router-dom",
-      //     "@mui/material": "@mui/material",
-      //   },
-      //   // assetFileNames: ({ name }) => {
-      //   //   if (/\.(gif|jpe?g|png|svg)$/.test(name ?? "")) {
-      //   //     return "assets/images/[name]-[hash][extname]";
-      //   //   }
-
-      //   //   if (/\.(otf|ttf)$/.test(name ?? "")) {
-      //   //     return "assets/fonts/[name]-[hash][extname]";
-      //   //   }
-
-      //   //   if (/\.css$/.test(name ?? "")) {
-      //   //     return "assets/css/[name]-[hash][extname]";
-      //   //   }
-
-      //   //   // default value
-      //   //   // ref: https://rollupjs.org/guide/en/#outputassetfilenames
-      //   //   return "assets/[name]-[hash][extname]";
-      //   // },
-      // },
     },
   },
   plugins: [react(), svgrPlugin(), dts()],
