84cbd8414a3402c4c6e9816aa05f96db11c496f5
robinsmedberg <robin.smedberg@gmail.com>
2023-03-15 20:19:52 +0100

    works to build

--- vite.config.ts
+++ vite.config.ts
@@ -1,47 +1,52 @@
 import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tsconfigPaths from "vite-tsconfig-paths";
 import svgrPlugin from "vite-plugin-svgr";
 import dts from "vite-plugin-dts";
+import { peerDependencies } from "./package.json";
+import typescript from "@rollup/plugin-typescript";
 
 export default defineConfig({
   build: {
     lib: {
       entry: path.resolve(__dirname, "src/index.ts"),
-      // name: "mui-components",
-      // fileName: "mui-components",
-      name: "mui-components",
-      fileName: (format) => `mui-components.${format}.js`,
+      name: "@agronod/mui-components",
+      fileName: (format) => `index.${format}.js`,
+      formats: ["es", "umd"],
     },
     rollupOptions: {
-      external: ["react", "react-dom", "react-router-dom", "@mui/material"],
-      output: {
-        globals: {
-          react: "React",
-          "react-dom": "react-dom",
-          "react-router-dom": "react-router-dom",
-          "@mui/material": "@mui/material",
-        },
-        // assetFileNames: ({ name }) => {
-        //   if (/\.(gif|jpe?g|png|svg)$/.test(name ?? "")) {
-        //     return "assets/images/[name]-[hash][extname]";
-        //   }
+      external: Object.keys(peerDependencies),
+      plugins: [
+        typescript({
+          tsconfig: "./tsconfig-build.json",
+        }),
+      ],
+      // output: {
+      //   globals: {
+      //     react: "React",
+      //     "react-dom": "react-dom",
+      //     "react-router-dom": "react-router-dom",
+      //     "@mui/material": "@mui/material",
+      //   },
+      //   // assetFileNames: ({ name }) => {
+      //   //   if (/\.(gif|jpe?g|png|svg)$/.test(name ?? "")) {
+      //   //     return "assets/images/[name]-[hash][extname]";
+      //   //   }
 
-        //   if (/\.(otf|ttf)$/.test(name ?? "")) {
-        //     return "assets/fonts/[name]-[hash][extname]";
-        //   }
+      //   //   if (/\.(otf|ttf)$/.test(name ?? "")) {
+      //   //     return "assets/fonts/[name]-[hash][extname]";
+      //   //   }
 
-        //   if (/\.css$/.test(name ?? "")) {
-        //     return "assets/css/[name]-[hash][extname]";
-        //   }
+      //   //   if (/\.css$/.test(name ?? "")) {
+      //   //     return "assets/css/[name]-[hash][extname]";
+      //   //   }
 
-        //   // default value
-        //   // ref: https://rollupjs.org/guide/en/#outputassetfilenames
-        //   return "assets/[name]-[hash][extname]";
-        // },
-      },
+      //   //   // default value
+      //   //   // ref: https://rollupjs.org/guide/en/#outputassetfilenames
+      //   //   return "assets/[name]-[hash][extname]";
+      //   // },
+      // },
     },
   },
-  plugins: [react(), tsconfigPaths(), svgrPlugin(), dts()],
+  plugins: [react(), svgrPlugin(), dts()],
 });
