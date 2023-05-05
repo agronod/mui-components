7f5fa9f6f0eecec674d2758acc6efb6f65007b26
Henrik <henrik.kindvall@agronod.com>
2023-05-05 08:37:59 +0200

    feat: custom header logo (#36)
    * feature: removed default agrosfar logo from Header. Added new prop for custom logo.

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 import { Stack, Toolbar, Link as MuiLink } from "@mui/material";
 import MuiAppBar, { AppBarProps as MuiAppBarProps } from "@mui/material/AppBar";
 import { styled } from "@mui/material/styles";
-import { AgrosfarPilotLogo, AgronodLogo } from "../../assets";
+import { AgronodLogo } from "../../assets";
 
 interface AppBarProps extends MuiAppBarProps {
   visible?: boolean;
@@ -26,13 +26,13 @@ interface IProps {
   /*  Child component with links for routing in the applications */
   menuLinks?: React.ReactNode | React.ReactNode[];
   /*  Boolean to show or hide the agronod logo */
-  agronodLogo?: boolean;
+  customLogo?: React.ReactNode | React.ReactNode[];
 }
 
 export default function Header({
   userDropdown,
   menuLinks,
-  agronodLogo,
+  customLogo,
 }: IProps) {
   return (
     <AppBar sx={{ height: 78 }} position="fixed" visible={true} elevation={1}>
@@ -55,7 +55,7 @@ export default function Header({
           maxHeight={30}
           maxWidth={166}
         >
-          {agronodLogo ? <AgronodLogo /> : <AgrosfarPilotLogo />}
+          {customLogo ? customLogo : <AgronodLogo />}
         </MuiLink>
         <Stack
           direction={"row"}
