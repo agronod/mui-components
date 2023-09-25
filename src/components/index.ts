766c12726c804c0c5dc03bbd28b912f186f63a44
Zvjezdana Visosevic <zvjezdanavisosevic@Zvjezdanas-MBP.lan>
2023-09-25 16:22:02 +0200

    export indicator

--- src/components/index.ts
+++ src/components/index.ts
@@ -8,6 +8,7 @@ export * from "./Loaders";
 export * from "./AsideCard";
 export * from "./ModalCard";
 export * from "./Tooltip";
+export * from "./ProgressIndicator";
 
 import { NyckeltalProps } from "./Nyckeltal";
 export type { NyckeltalProps };
