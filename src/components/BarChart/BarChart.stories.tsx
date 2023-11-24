eab4c507ced37a6bb23bb8be8c53e8790fb91889
Emil Rondahl <emil.rondahl@agronod.com>
2023-11-24 14:13:06 +0100

    feat: enable stacked bar charts (#72)
    * feat: enable stacked bar charts
    
    * 1.8.0-feat-enable-stacked-bar-chart.0
    
    ---------
    
    Co-authored-by: GitHub Action <action@github.com>

--- src/components/BarChart/BarChart.stories.tsx
+++ src/components/BarChart/BarChart.stories.tsx
@@ -33,6 +33,21 @@ const chartData =
       value: 2290,
     },
   ] || [];
+const chartDataStacked =
+  [
+    {
+      name: "Page A",
+      value: [2400, 2000],
+    },
+    {
+      name: "Page B",
+      value: [2500, 2000],
+    },
+    {
+      name: "Page C",
+      value: [2400, 1000],
+    },
+  ] || [];
 
 export const BarChartDefault: StoryFn<typeof BarChart> = ({ ...args }) => (
   <Box sx={{ height: "400px", width: "600px" }}>
@@ -69,6 +84,54 @@ BarChartDefault.args = {
         >
           <Box>{label}</Box>
           <Box>{payload[0].value}</Box>
+        </Box>
+      );
+    }
+
+    return null;
+  },
+};
+
+export const BarChartStacked: StoryFn<typeof BarChart> = ({ ...args }) => (
+  <Box sx={{ height: "400px", width: "600px" }}>
+    <BarChart {...args} />
+  </Box>
+);
+
+BarChartStacked.args = {
+  data: chartDataStacked,
+  barOptions: {
+    radius: [20, 20, 0, 0],
+  },
+  barCellsOptions: [...chartData].map(() => [
+    {
+      fill: "#123F25",
+    },
+    {
+      fill: "#59b87f",
+    },
+  ]),
+  yAxisOptions: {
+    hide: true,
+  },
+  xAxisOptions: {
+    height: 54,
+    tickMargin: 20,
+    interval: 0,
+  },
+  TooltipContent: ({ active, payload, label }: any) => {
+    if (active && payload && payload.length) {
+      return (
+        <Box
+          sx={{
+            backgroundColor: "white",
+            padding: "16px",
+            borderRadius: "8px",
+            boxShadow: "0px 2px 4px rgba(0, 0, 0, 0.05)",
+          }}
+        >
+          <Box>{label}</Box>
+          <Box>{payload.reduce((acc: number, p: any) => acc + p.value, 0)}</Box>
         </Box>
       );
     }
