3d509aa696e8fc4bb22fc632a6bca3e6b1ff3279
Zvjezdana Visosevic <zvjezdana.visosevic@agronod.com>
2024-03-12 17:53:33 +0100

    fix props

--- src/components/AgronodCard/AgronodCard.tsx
+++ src/components/AgronodCard/AgronodCard.tsx
@@ -1,11 +1,11 @@
 import { SxProps, Theme, Card as MuiCard } from "@mui/material";
 
-interface Props {
+interface AgronodCardProps {
   children: JSX.Element | JSX.Element[];
   sx?: SxProps<Theme>;
 }
 
-const AgronodCard = (props: Props) => {
+const AgronodCard = (props: AgronodCardProps) => {
   return (
     <MuiCard sx={[props.sx as (theme: Theme) => any]} variant="outlined">
       {props.children}
