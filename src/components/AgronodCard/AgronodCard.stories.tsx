da8063ac8501f753429114ea2c792cad26a8e444
Zvjezdana Visosevic <zvjezdana.visosevic@agronod.com>
2024-03-12 17:49:54 +0100

    small fix

--- src/components/AgronodCard/AgronodCard.stories.tsx
+++ src/components/AgronodCard/AgronodCard.stories.tsx
@@ -34,7 +34,11 @@ export const AgronodCardDefault: StoryFn<typeof AgronodCard> = ({
   sx,
   ...rest
 }) => {
-  return <AgronodCard {...rest}>{children}</AgronodCard>;
+  return (
+    <AgronodCard {...rest} sx={sx}>
+      {children}
+    </AgronodCard>
+  );
 };
 AgronodCardDefault.args = {
   children: (
@@ -51,4 +55,36 @@ AgronodCardDefault.args = {
       </Typography>
     </Stack>
   ),
+};
+
+export const AgronodCardModifiedStyles: StoryFn<typeof AgronodCard> = ({
+  children,
+  sx,
+  ...rest
+}) => {
+  return (
+    <AgronodCard {...rest} sx={sx}>
+      {children}
+    </AgronodCard>
+  );
+};
+AgronodCardModifiedStyles.args = {
+  children: (
+    <Stack gap={2}>
+      <Typography variant="h2">Lorem Ipsum</Typography>
+      <Typography variant="body1">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum.
+      </Typography>
+    </Stack>
+  ),
+  sx: (theme) => ({
+    backgroundColor: theme.palette.primary.light,
+    borderColor: theme.palette.primary.dark,
+  }),
 };
