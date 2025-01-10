eeda40dfb8596c1cf455d32a16b5665cf86804c6
Tolga Köyluoglu <tolga.koyluoglu1@gmail.com>
2025-01-10 11:36:05 +0100

    fix: add sx prop to checkbox

--- src/components/AgronodCheckbox/AgronodCheckbox.tsx
+++ src/components/AgronodCheckbox/AgronodCheckbox.tsx
@@ -4,6 +4,9 @@ import {
   FormControlLabel,
   Typography,
   Box,
+  Theme,
+  SxProps,
+  useTheme,
 } from "@mui/material";
 import { ReactNode } from "react";
 import { isSafari } from "react-device-detect";
@@ -17,45 +20,51 @@ export interface AgronodCheckboxProps extends AgronodMuiCheckboxBaseProps {
   label?: ReactNode;
   noPadding?: boolean;
   description?: string;
+  sx?: SxProps<Theme>;
 }
 
-const StyledMuiCheckbox = (props: AgronodCheckboxProps) => (
-  <MuiCheckbox
-    disabled={props.disabled}
-    onChange={props.onChange}
-    onClick={props.onClick}
-    color={props.color}
-    checked={props.checked}
-    disableRipple={true}
-    sx={(theme) => ({
-      color: theme.palette.input.border,
-      padding: props.noPadding === true ? 0 : "9px",
-      paddingY: props.size === "small" || props.noPadding === true ? 0 : "9px",
-      display: "inline-grid",
-      "&.MuiCheckbox-root input": isSafari ? { pointerEvents: "auto" } : null,
+const StyledMuiCheckbox = (props: AgronodCheckboxProps) => {
+  const theme = useTheme();
+  return (
+    <MuiCheckbox
+      disabled={props.disabled}
+      onChange={props.onChange}
+      onClick={props.onClick}
+      color={props.color}
+      checked={props.checked}
+      disableRipple={true}
+      sx={{
+        color: theme.palette.input.border,
+        padding: props.noPadding === true ? 0 : "9px",
+        paddingY:
+          props.size === "small" || props.noPadding === true ? 0 : "9px",
+        display: "inline-grid",
+        "&.MuiCheckbox-root input": isSafari ? { pointerEvents: "auto" } : null,
 
-      "&.Mui-disabled": {
-        color: theme.palette.border,
-      },
-
-      "&.Mui-focusVisible": {
-        "& path": {
-          outline: `1px solid ${theme.palette.text.primary} !important`,
-          borderRadius: "1px",
+        "&.Mui-disabled": {
+          color: theme.palette.border,
         },
-      },
 
-      "& .MuiSvgIcon-root": {
-        // since svg has some space in relation to path, this size matches 24px looks
-        fontSize: "32px",
-
-        [theme.breakpoints.down("md")]: {
-          fontSize: props.size === "small" ? "32px" : "42.67px", // since svg has some space in relation to path, 42.67px size matches 32px looks
+        "&.Mui-focusVisible": {
+          "& path": {
+            outline: `1px solid ${theme.palette.text.primary} !important`,
+            borderRadius: "1px",
+          },
         },
-      },
-    })}
-  />
-);
+
+        "& .MuiSvgIcon-root": {
+          // since svg has some space in relation to path, this size matches 24px looks
+          fontSize: "32px",
+
+          [theme.breakpoints.down("md")]: {
+            fontSize: props.size === "small" ? "32px" : "42.67px", // since svg has some space in relation to path, 42.67px size matches 32px looks
+          },
+        },
+        ...props.sx,
+      }}
+    />
+  );
+};
 
 const AgronodCheckbox = (props: AgronodCheckboxProps) => {
   return (
