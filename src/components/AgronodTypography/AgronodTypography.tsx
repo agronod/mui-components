5742bf50764b59c854abb09b9749c74d328d7ea8
Jonas Persson <jpemts@gmail.com>
2025-10-13 11:34:24 +0200

    wip: added forwardRef to agronod typography

--- src/components/AgronodTypography/AgronodTypography.tsx
+++ src/components/AgronodTypography/AgronodTypography.tsx
@@ -2,11 +2,14 @@ import {
   Typography as MuiTypography,
   TypographyProps as MuiTypographyProps,
 } from "@mui/material";
+import { forwardRef } from "react";
 
 export type AgronodTypographyProps = MuiTypographyProps;
 
-const AgronodTypography = (props: AgronodTypographyProps) => (
-  <MuiTypography {...props} />
+const AgronodTypography = forwardRef<HTMLSpanElement, AgronodTypographyProps>(
+  (props, ref) => <MuiTypography ref={ref} {...props} />
 );
+
+AgronodTypography.displayName = "AgronodTypography";
 
 export default AgronodTypography;
